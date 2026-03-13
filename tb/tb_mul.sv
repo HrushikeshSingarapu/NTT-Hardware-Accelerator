9e7d199c18013356403c81dbeb4303d2f36484b3
Hrushikesh Singarapu <hrushikeshsingarapu@gmail.com>
2026-03-13 22:58:21 +0530

    in tb_mul y->result

--- tb/tb_mul.sv
+++ tb/tb_mul.sv
@@ -19,28 +19,28 @@ module tb_mul;
 
         // both zero
         a = 0; b = 0; #10;
-        if (result == (a * b) % 3329) $display("PASS | a=%0d b=%0d | y=%0d", a, b, y);
-        else $display("FAIL | a=%0d b=%0d | expected=%0d got=%0d", a, b, (a*b)%3329, y);
+        if (result == (a * b) % 3329) $display("PASS | a=%0d b=%0d |result=%0d", a, b,result);
+        else $display("FAIL | a=%0d b=%0d | expected=%0d got=%0d", a, b, (a*b)%3329,result);
 
         // one zero
         a = 3328; b = 0; #10;
-        if (result == (a * b) % 3329) $display("PASS | a=%0d b=%0d | y=%0d", a, b, y);
-        else $display("FAIL | a=%0d b=%0d | expected=%0d got=%0d", a, b, (a*b)%3329, y);
+        if (result == (a * b) % 3329) $display("PASS | a=%0d b=%0d |result=%0d", a, b,result);
+        else $display("FAIL | a=%0d b=%0d | expected=%0d got=%0d", a, b, (a*b)%3329,result);
 
         // max * max
         a = 3328; b = 3328; #10;
-        if (result == (a * b) % 3329) $display("PASS | a=%0d b=%0d | y=%0d", a, b, y);
-        else $display("FAIL | a=%0d b=%0d | expected=%0d got=%0d", a, b, (a*b)%3329, y);
+        if (result == (a * b) % 3329) $display("PASS | a=%0d b=%0d |result=%0d", a, b,result);
+        else $display("FAIL | a=%0d b=%0d | expected=%0d got=%0d", a, b, (a*b)%3329,result);
 
         // small values
         a = 17; b = 17; #10;
-        if (result == (a * b) % 3329) $display("PASS | a=%0d b=%0d | y=%0d", a, b, y);
-        else $display("FAIL | a=%0d b=%0d | expected=%0d got=%0d", a, b, (a*b)%3329, y);
+        if (result == (a * b) % 3329) $display("PASS | a=%0d b=%0d |result=%0d", a, b,result);
+        else $display("FAIL | a=%0d b=%0d | expected=%0d got=%0d", a, b, (a*b)%3329,result);
 
         // result in range check
         a = 475; b = 7; #10;
-        if (result >= 3329) $display("FAIL | out of range | got=%0d", y);
-        else $display("PASS | in range | y=%0d", y);
+        if (result >= 3329) $display("FAIL | out of range | got=%0d",result);
+        else $display("PASS | in range |result=%0d",result);
 
         #50 $finish;
     end
